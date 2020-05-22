490cef175e1478a6fe722813a3c475d4cbcd441e
Bruno Bartol <bruno.bartol1609@gmail.com>
2020-05-22 21:53:57 +0200

    Initial Commit

---
+++ CrossTalk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CrossTalk
+//
+//  Created by Bruno  Bartol on 22/05/2020.
+//  Copyright © 2020 Bruno  Bartol. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
